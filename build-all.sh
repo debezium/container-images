6b6f54d0ab45096e652a743272e5fddf38129ac7
Jiri Pechanec <jpechane@redhat.com>
2017-11-03 09:53:06 +0100

    DBZ-426 PostgreSQL 10.0 Image

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 JAVA_VERSION=8u121
 DEBEZIUM_VERSION=0.6
 MONGO_VERSION=3.2
-POSTGRES_VERSION=9.6
+POSTGRES_VERSIONS="9.6 10.0"
 
 ./build-java.sh $JAVA_VERSION
 if [ $? -ne 0 ]; then
@@ -15,9 +15,11 @@ if [ $? -ne 0 ]; then
     exit $?;
 fi
 
-./build-postgres.sh $POSTGRES_VERSION
-if [ $? -ne 0 ]; then
-    exit $?;
-fi
+for i in $POSTGRES_VERSIONS; do
+  ./build-postgres.sh $i
+  if [ $? -ne 0 ]; then
+      exit $?;
+  fi
+done
 
 ./build-debezium.sh $DEBEZIUM_VERSION
