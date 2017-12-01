8eb5d9bd60142f259afcf80e99f1cac548c3a917
Gunnar Morling <gunnar.morling@googlemail.com>
2017-12-01 23:00:35 +0100

    DBZ-470 Inserting example data using longs for ids and int for quantity instead of float

--- examples/mongodb/0.7/init-inventory.sh
+++ examples/mongodb/0.7/init-inventory.sh
@@ -40,29 +40,29 @@ mongo -u debezium -p dbz --authenticationDatabase admin localhost:27017/inventor
     use inventory;
 
     db.products.insert([
-        { _id : 101, name : 'scooter', description: 'Small 2-wheel scooter', weight : 3.14, quantity : 3 },
-        { _id : 102, name : 'car battery', description: '12V car battery', weight : 8.1, quantity : 8 },
-        { _id : 103, name : '12-pack drill bits', description: '12-pack of drill bits with sizes ranging from #40 to #3', weight : 0.8, quantity : 18 },
-        { _id : 104, name : 'hammer', description: "12oz carpenter's hammer", weight : 0.75, quantity : 4 },
-        { _id : 105, name : 'hammer', description: "14oz carpenter's hammer", weight : 0.875, quantity : 5 },
-        { _id : 106, name : 'hammer', description: "16oz carpenter's hammer", weight : 1.0, quantity : 0 },
-        { _id : 107, name : 'rocks', description: 'box of assorted rocks', weight : 5.3, quantity : 44 },
-        { _id : 108, name : 'jacket', description: 'water resistent black wind breaker', weight : 0.1, quantity : 2 },
-        { _id : 109, name : 'spare tire', description: '24 inch spare tire', weight : 22.2, quantity : 5 }
+        { _id : NumberLong("101"), name : 'scooter', description: 'Small 2-wheel scooter', weight : 3.14, quantity : NumberInt("3") },
+        { _id : NumberLong("102"), name : 'car battery', description: '12V car battery', weight : 8.1, quantity : NumberInt("8") },
+        { _id : NumberLong("103"), name : '12-pack drill bits', description: '12-pack of drill bits with sizes ranging from #40 to #3', weight : 0.8, quantity : NumberInt("18") },
+        { _id : NumberLong("104"), name : 'hammer', description: "12oz carpenter's hammer", weight : 0.75, quantity : NumberInt("4") },
+        { _id : NumberLong("105"), name : 'hammer', description: "14oz carpenter's hammer", weight : 0.875, quantity : NumberInt("5") },
+        { _id : NumberLong("106"), name : 'hammer', description: "16oz carpenter's hammer", weight : 1.0, quantity : NumberInt("0") },
+        { _id : NumberLong("107"), name : 'rocks', description: 'box of assorted rocks', weight : 5.3, quantity : NumberInt("44") },
+        { _id : NumberLong("108"), name : 'jacket', description: 'water resistent black wind breaker', weight : 0.1, quantity : NumberInt("2") },
+        { _id : NumberLong("109"), name : 'spare tire', description: '24 inch spare tire', weight : 22.2, quantity : NumberInt("5") }
     ]);
 
     db.customers.insert([
-        { _id : 1001, first_name : 'Sally', last_name : 'Thomas', email : 'sally.thomas@acme.com' },
-        { _id : 1002, first_name : 'George', last_name : 'Bailey', email : 'gbailey@foobar.com' },
-        { _id : 1003, first_name : 'Edward', last_name : 'Walker', email : 'ed@walker.com' },
-        { _id : 1004, first_name : 'Anne', last_name : 'Kretchmar', email : 'annek@noanswer.org' }
+        { _id : NumberLong("1001"), first_name : 'Sally', last_name : 'Thomas', email : 'sally.thomas@acme.com' },
+        { _id : NumberLong("1002"), first_name : 'George', last_name : 'Bailey', email : 'gbailey@foobar.com' },
+        { _id : NumberLong("1003"), first_name : 'Edward', last_name : 'Walker', email : 'ed@walker.com' },
+        { _id : NumberLong("1004"), first_name : 'Anne', last_name : 'Kretchmar', email : 'annek@noanswer.org' }
     ]);
 
     db.orders.insert([
-        { _id : 10001, order_date : new ISODate("2016-01-16T00:00:00Z"), purchaser_id : 1001, quantity : 1, product_id : 102 },
-        { _id : 10002, order_date : new ISODate("2016-01-17T00:00:00Z"), purchaser_id : 1002, quantity : 2, product_id : 105 },
-        { _id : 10003, order_date : new ISODate("2016-02-19T00:00:00Z"), purchaser_id : 1002, quantity : 2, product_id : 106 },
-        { _id : 10004, order_date : new ISODate("2016-02-21T00:00:00Z"), purchaser_id : 1003, quantity : 1, product_id : 107 }
+        { _id : NumberLong("10001"), order_date : new ISODate("2016-01-16T00:00:00Z"), purchaser_id : NumberLong("1001"), quantity : NumberInt("1"), product_id : NumberLong("102") },
+        { _id : NumberLong("10002"), order_date : new ISODate("2016-01-17T00:00:00Z"), purchaser_id : NumberLong("1002"), quantity : NumberInt("2"), product_id : NumberLong("105") },
+        { _id : NumberLong("10003"), order_date : new ISODate("2016-02-19T00:00:00Z"), purchaser_id : NumberLong("1002"), quantity : NumberInt("2"), product_id : NumberLong("106") },
+        { _id : NumberLong("10004"), order_date : new ISODate("2016-02-21T00:00:00Z"), purchaser_id : NumberLong("1003"), quantity : NumberInt("1"), product_id : NumberLong("107") }
     ]);
 EOF
 
