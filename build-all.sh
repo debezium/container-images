2dc5972064c4caadfe2a7c6490c697ac8e20db82
Jiri Pechanec <jpechane@redhat.com>
2018-01-04 14:05:15 +0100

    DBZ-449 Remove JDK images

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-JAVA_VERSION=8u121
 DEBEZIUM_VERSION=0.7
 MONGO_VERSION=3.2
 POSTGRES_VERSIONS="9.6 10.0"
-
-./build-java.sh $JAVA_VERSION
-if [ $? -ne 0 ]; then
-    exit $?;
-fi
 
 ./build-mongo.sh $MONGO_VERSION
 if [ $? -ne 0 ]; then
