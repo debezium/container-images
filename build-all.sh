3d06bcae3a811be28a586643927bf0344c3eed29
Jiri Pechanec <jpechane@redhat.com>
2019-10-20 08:13:25 +0200

    DBZ-1542 Support for Postgres 12 images

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 DEBEZIUM_VERSION="1.0"
 MONGO_VERSIONS="3.2 3.4 3.6 4.0"
-POSTGRES_VERSIONS="9.6 9.6-alpine 10 10-alpine 11 11-alpine"
+POSTGRES_VERSIONS="9.6 9.6-alpine 10 10-alpine 11 11-alpine 12 12-alpine"
 
 for MONGO_VERSION in $MONGO_VERSIONS; do
   ./build-mongo.sh "$MONGO_VERSION"
