0f1c3872deed5ba0df14ec7a7db65117b1bb7d86
Jiri Pechanec <jpechane@redhat.com>
2021-02-17 11:01:18 +0100

    DBZ-3027 Add Debezium UI image

--- build-debezium.sh
+++ build-debezium.sh
@@ -51,6 +51,7 @@ build_docker_image kafka
 build_docker_image connect-base
 build_docker_image connect
 build_docker_image server
+build_docker_image ui
 build_docker_image example-mysql examples/mysql
 build_docker_image example-mysql-gtids examples/mysql-gtids
 build_docker_image example-postgres examples/postgres
