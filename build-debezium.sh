0c19ec1b8cb43bf49d78402aabc53c1929b82d9c
Jiri Pechanec <jpechane@redhat.com>
2021-03-12 09:54:29 +0100

    [release] Allow to skip UI build

--- build-debezium.sh
+++ build-debezium.sh
@@ -51,7 +51,9 @@ build_docker_image kafka
 build_docker_image connect-base
 build_docker_image connect
 build_docker_image server
-build_docker_image ui
+if [[ "$SKIP_UI" != "true" ]]; then
+    build_docker_image ui
+fi
 build_docker_image example-mysql examples/mysql
 build_docker_image example-mysql-gtids examples/mysql-gtids
 build_docker_image example-postgres examples/postgres
