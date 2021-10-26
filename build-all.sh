4fd900066baf58d0e326bc287c26246f93b92d5b
Anisha Mohanty <anishamohanty23@gmail.com>
2021-10-26 18:30:33 +0530

    DBZ-4195 DBZ-3691 Re-enable building alpine images

--- build-all.sh
+++ build-all.sh
@@ -7,7 +7,7 @@ if [ -z "$DEBEZIUM_VERSIONS" ]; then
   DEBEZIUM_VERSIONS="$DEBEZIUM_VERSION"
 fi
 MONGO_VERSIONS="3.2 3.4 3.6 4.0 4.2 5.0"
-POSTGRES_VERSIONS="9.6 10 11 12 13 14 14-alpine"
+POSTGRES_VERSIONS="9.6 9.6-alpine 10 10-alpine 11 11-alpine 12 12-alpine 13 13-alpine 14 14-alpine"
 
 for MONGO_VERSION in $MONGO_VERSIONS; do
   ./build-mongo.sh "$MONGO_VERSION"
