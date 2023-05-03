add115cfbd06eaa77e59266ba53897b433a717db
Chris Cranford <chris@hibernate.org>
2023-05-03 13:36:02 -0400

    [ci] Reordered image builds

--- build-debezium.sh
+++ build-debezium.sh
@@ -102,10 +102,10 @@ build_docker_image connect
 build_docker_image server
 build_docker_image example-mysql examples/mysql
 build_docker_image example-mysql-gtids examples/mysql-gtids
-build_docker_image example-mysql-master examples/mysql-replication/master
-build_docker_image example-mysql-replica examples/mysql-replication/replica
 build_docker_image example-postgres examples/postgres
 build_docker_image example-mongodb examples/mongodb
+build_docker_image example-mysql-master examples/mysql-replication/master
+build_docker_image example-mysql-replica examples/mysql-replication/replica
 if [[ "$SKIP_UI" != "true" ]]; then
     build_docker_image debezium-ui ui
 fi
