91bc5e92712733ec593a76f61e864be4f41e93ea
Robin Tang <rtang.cs@gmail.com>
2023-11-30 17:03:01 -0800

    Update setup_certs.

--- server/2.1/setup_certs.sh
+++ server/2.1/setup_certs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# IMPORTANT: DO NOT MODIFY THIS SCRIPT, THIS WAS COPIED FROM: https://docs.aws.amazon.com/documentdb/latest/developerguide/connect_programmatically.html#w139aac29c11c13b5b9
+
+# IMPORTANT: This script is copied from: https://docs.aws.amazon.com/documentdb/latest/developerguide/connect_programmatically.html#w139aac29c11c13b5b9 
+# and modified to inject the certificates into Java's default cacert
 mydir=/tmp/certs
-truststore=${mydir}/rds-truststore.jks
-storepassword=artie123
+truststore=$JAVA_HOME/lib/security/cacerts
+storepassword=changeit
 
 curl -sS "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" > ${mydir}/global-bundle.pem
 awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1}{print > "rds-ca-" n ".pem"}' < ${mydir}/global-bundle.pem
@@ -10,17 +12,6 @@ awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_aft
 for CERT in rds-ca-*; do
   alias=$(openssl x509 -noout -text -in $CERT | perl -ne 'next unless /Subject:/; s/.*(CN=|CN = )//; print')
   echo "Importing $alias"
-  keytool -import -file ${CERT} -alias "${alias}" -storepass ${storepassword} -keystore ${truststore} -noprompt
+  keytool -trustcacerts -import -file ${CERT} -alias "${alias}" -storepass ${storepassword} -keystore ${truststore} -noprompt
   rm $CERT
 done
-
-rm ${mydir}/global-bundle.pem
-
-echo "Trust store content is: "
-
-keytool -list -v -keystore "$truststore" -storepass ${storepassword} | grep Alias | cut -d " " -f3- | while read alias
-do
-   expiry=`keytool -list -v -keystore "$truststore" -storepass ${storepassword} -alias "${alias}" | grep Valid | perl -ne 'if(/until: (.*?)\n/) { print "$1\n"; }'`
-   echo " Certificate ${alias} expires in '$expiry'"
-done
-
