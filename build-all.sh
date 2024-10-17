9c50d1ea2c872e89c93b93d54487320f11644611
Jiri Pechanec <jpechane@redhat.com>
2024-10-17 07:17:07 +0200

    DBZ-8275 Add PostgreSQL 17 to legacy scripts

--- build-all.sh
+++ build-all.sh
@@ -7,7 +7,7 @@ if [ -z "$DEBEZIUM_VERSIONS" ]; then
   DEBEZIUM_VERSIONS="$DEBEZIUM_VERSION"
 fi
 MONGO_VERSIONS="3.2 3.4 3.6 4.0 4.2 4.4 5.0 6.0"
-POSTGRES_VERSIONS="12 12-alpine 13 13-alpine 14 14-alpine 15 15-alpine 16 16-alpine"
+POSTGRES_VERSIONS="12 12-alpine 13 13-alpine 14 14-alpine 15 15-alpine 16 16-alpine 17 17-alpine"
 
 for MONGO_VERSION in $MONGO_VERSIONS; do
   ./build-mongo.sh "$MONGO_VERSION"
