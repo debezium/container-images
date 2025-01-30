1232c32f9c26a8872c8f48527027e72613f27ca4
Fiore Mario Vitale <mvitale@redhat.com>
2025-01-30 10:26:03 +0100

    DBZ-8605 Add conductor to build scripts

--- build-debezium-multiplatform.sh
+++ build-debezium-multiplatform.sh
@@ -125,6 +125,7 @@ build_docker_image connect-base
 build_docker_image connect
 build_docker_image server
 build_docker_image operator
+build_docker_image conductor
 if [[ "$SKIP_UI" != "true" ]]; then
     build_docker_image debezium-ui ui
 fi
