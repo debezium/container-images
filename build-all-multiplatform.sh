dc244ab7a4d17fee1eb15c03e02bdc7794344456
Jiri Pechanec <jpechane@redhat.com>
2025-02-26 11:21:18 +0100

    DBZ-8713 Build multiarch images first

--- build-all-multiplatform.sh
+++ build-all-multiplatform.sh
@@ -32,13 +32,14 @@ DEBEZIUM_SINGLEPLATFORM_VERSIONS="1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8"
 POSTGRES_VERSIONS="12 13 14"
 POSTGRES_MULTIPLATFORM_VERSIONS="15 16 17 12-alpine 13-alpine 14-alpine 15-alpine 16-alpine 17-alpine"
 
+docker buildx prune -f || true
 if shouldBuild "postgres"; then
-  for POSTGRES_VERSION in $POSTGRES_VERSIONS; do
-    ./build-postgres-multiplatform.sh "$POSTGRES_VERSION" "linux/amd64"
-  done
-
   for POSTGRES_VERSION in $POSTGRES_MULTIPLATFORM_VERSIONS; do
     ./build-postgres-multiplatform.sh "$POSTGRES_VERSION" "${MULTIPLATFORM_PLATFORMS}"
+  done
+
+  for POSTGRES_VERSION in $POSTGRES_VERSIONS; do
+    ./build-postgres-multiplatform.sh "$POSTGRES_VERSION" "linux/amd64"
   done
 fi;
 
