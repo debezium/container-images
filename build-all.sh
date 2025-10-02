1e438329cbb42b7c85d3d1e079b250df561bff29
Jiri Pechanec <jiri.pechanec@centrum.cz>
2025-10-02 12:16:20 +0200

    DBZ-9523 Images for PostgreSQL 18

--- build-all.sh
+++ build-all.sh
@@ -6,7 +6,7 @@ DEBEZIUM_VERSION="3.3"
 if [ -z "$DEBEZIUM_VERSIONS" ]; then
   DEBEZIUM_VERSIONS="$DEBEZIUM_VERSION"
 fi
-POSTGRES_VERSIONS="12 12-alpine 13 13-alpine 14 14-alpine 15 15-alpine 16 16-alpine 17 17-alpine"
+POSTGRES_VERSIONS="12 12-alpine 13 13-alpine 14 14-alpine 15 15-alpine 16 16-alpine 17 17-alpine 18 18-alpine"
 
 for POSTGRES_VERSION in $POSTGRES_VERSIONS; do
   ./build-postgres.sh "$POSTGRES_VERSION"
