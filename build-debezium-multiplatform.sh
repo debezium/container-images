b9eebbdbba0d46b71638d0d6bcfe00bda975250f
kmos <kmos@commonhaus.dev>
2026-09-17 02:01:58 +0200

    debezium/dbz#2518 add regular container release for native build
    Signed-off-by: kmos <kmos@commonhaus.dev>

--- build-debezium-multiplatform.sh
+++ build-debezium-multiplatform.sh
@@ -145,6 +145,7 @@ build_docker_image kafka
 build_docker_image connect-base
 build_docker_image connect
 build_docker_image server
+build_docker_image server-native
 build_docker_image operator
 if [[ "$SKIP_UI" != "true" ]]; then
     build_docker_image platform-conductor
